a0e1cf9b416258c1d3b31396ca973e6a2f0c336f
Luis22Ant <luisengenhariaeletronica@gmail.com>
2024-06-14 16:40:09 -0300

    Ajuste autorização

--- BlazorEngenharia/BlazorEngenharia/Program.cs
+++ BlazorEngenharia/BlazorEngenharia/Program.cs
@@ -1,4 +1,5 @@
 using BlazorEngenharia;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -11,6 +12,9 @@ public class Program
     {
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
         builder.RootComponents.Add<App>("#app");
+
+        builder.Services.AddAuthorizationCore();
+        builder.Services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
 
         // Configurar HttpClient com suporte a autenticação
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
---
+++ BlazorEngenharia/BlazorEngenharia/ServerAuthenticationStateProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorEngenharia
+{
+    public class ServerAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+          
+            var identity = new ClaimsIdentity(); 
+
+            var user = new ClaimsPrincipal(identity);
+
+            return Task.FromResult(new AuthenticationState(user));
+        }
+    }
+}
